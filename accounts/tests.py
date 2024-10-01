8392cbadbb2338650f5cd1a284c9030fbb19cb0e
LertonJosine <josinelerton@gmail.com>
2024-10-01 19:16:45 +0200

    funcionalidades login, logout e signup implementadas

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import resolve, reverse
+from .forms import CustomUserCreationForm
+from .views import SignupView
 
 class AccountsTest(TestCase):
     
@@ -18,3 +21,26 @@ class AccountsTest(TestCase):
         self.assertEqual(super_user.email, 'jozine@gmail.com')
         self.assertTrue(super_user.is_superuser)
 
+
+class SignupTest(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+    
+    
+    def test_signup_url(self):
+        self.assertEqual(self.response.status_code, 200)
+    
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+    
+    def test_signup_view(self):
+        view = resolve(reverse('signup'))
+        self.assertEqual(view.func.__name__, SignupView.as_view().__name__)
+    
+    def test_signup_template(self):
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        
+    

cd4cf62547ca45d819f369302fad8aa8625ab855
LertonJosine <josinelerton@gmail.com>
2024-10-11 20:46:32 +0200

    paginas about e trainers criadas

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, AboutPageView, TrainersPageView
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('trainers/', TrainersPageView.as_view(), name='trainers'),
 ]

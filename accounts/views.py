fb2135c9cd4a2a90977e8c98f7821c64882e8080
LertonJosine <josinelerton@gmail.com>
2024-10-12 10:08:56 +0200

    restrições aplicadas para a pagina de perfile e estilização das paginas de redefinição de password e mudação de password

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.views.generic import CreateView, UpdateView
 from django.contrib.auth import get_user_model
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 class SignupView(CreateView):
@@ -10,8 +11,12 @@ class SignupView(CreateView):
     template_name = 'registration/signup.html'
 
 
-class ProfileView(UpdateView):
+class ProfileView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = get_user_model()
     form_class = CustomUserChangeForm
     template_name = 'registration/profile.html'
+    
+    def test_func(self):
+        user = self.request.user
+        return user == self.get_object()
     
